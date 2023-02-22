64c63fe1a32bd1df2ff9757c87486a175e00afd4
Ariel Carvalho <arielcarvnasc@gmail.com>
2023-02-22 00:01:41 -0300

    WIP: client and server communication

--- client.c
+++ client.c
@@ -11,15 +11,39 @@
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+void sig_handler(int signum)
+{
+	//Return type of the handler function should be void
+	printf("\nInside handler function\n");
+}
 
 int	main(int argc, char **argv)
 {
-	if (argc != 1)
+	int pid = atoi(argv[1]);
+	int	i = 0;
+
+	printf("PID:%d\n", pid);
+	if (argc != 3)
 	{
+		printf("Wrong parameters quantity! Where is the string?\n");
 		printf("ARGC=%d\n", argc);
+		return(0);
 	}
+	signal(SIGUSR1, sig_handler);
+	while (1)
+	{
+		printf("%d : Inside main function\n", i++);
+		kill(pid, SIGUSR1);
+		sleep(1);  // Delay for 1 second
+	}
+	printf("ARGC=%d\n", argc);
 	printf("ARGV[0]:%s\n", argv[0]);
 	printf("ARGV[1]:%s\n", argv[1]);
-	printf("ARGV[2]:%s", argv[2]);
+	printf("ARGV[2]:%s\n", argv[2]);
+	printf("ARGV[3]:%s\n", argv[3]);
 	return (0);
 }
--- server.c
+++ server.c
@@ -15,27 +15,27 @@
 //signal macros and kill() prototype
 #include <signal.h>
 
-void	handle_signal(signal)
+void	handle_signal(int signal)
 {
-	printf("%d", signal);
+	printf("Inside the handler. Signal number:%d\n", signal);
 	return ;
 }
 
 int	main(int argc, char **argv)
 {
 	int	pid;
+	int	i = 0;
 
 	(void)argv;
-    if (argc != 1)
-    {
+	if (argc != 1)
+	{
 		printf("Error!\n");
 		return (0);
-    }
+	}
 	pid = getpid();
 	while (1)
 	{
-		printf("LOOP. PID Process: %d\n", pid);
-		sleep(1);
+		printf("%d: LOOP. PID Process: %d\n", i++, pid);
 		signal(SIGUSR1, handle_signal);
 		sleep(1);
 	}
