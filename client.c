691502a6da2bb7fcbaab45e86a606723968d2e4c
Ariel Carvalho Nascimento <arielcarvnasc@gmail.com>
2023-02-25 21:16:07 -0300

    WIP: receiving signals, but bugs still

--- client.c
+++ client.c
@@ -6,44 +6,50 @@
 /*   By: arcarval <arcarval@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 19:48:28 by arcarval          #+#    #+#             */
-/*   Updated: 2023/02/23 14:42:38 by arcarval         ###   ########.fr       */
+/*   Updated: 2023/02/25 21:14:17 by arcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Libft/libft.h"
-
 #include <stdio.h>
 #include <signal.h>
 
-void	sig_handler(int signum)
+void	send_char(int pid, char character)
 {
-	//Return type of the handler function should be void
-	printf("\nInside handler function: %d\n", signum);
+	int	bit;
+
+	// printf("Inside handler function: %d - %c\n", pid, character);
+	bit = 0;
+	while (bit < 8)
+	{
+		if ((character & (0x01 << bit)) != 0)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(150);
+		bit++;
+	}
 }
 
 int	main(int argc, char **argv)
 {
-	int pid = atoi(argv[1]);
-	int	i = 0;
+	int	pid;
+	int	index;
 
-	printf("PID:%d\n", pid);
 	if (argc != 3)
 	{
-		printf("Wrong parameters quantity! Where is the string?\n");
-		printf("ARGC=%d\n", argc);
+		printf("Wrong parameters quantity %d! Where is the string?\n", argc);
 		return (0);
 	}
-	signal(SIGUSR1, sig_handler);
-	while (1)
+	pid = ft_atoi(argv[1]);
+	printf("PID:%d\n", pid);
+	index = 0;
+	while (argv[2][index])
 	{
-		printf("%d : Inside main function\n", i++);
-		kill(pid, SIGUSR1);
-		sleep (1);  // Delay for 1 second
+		printf("%d : Inside main function.", index);
+		send_char(pid, argv[2][index]);
+		index++;
 	}
-	printf("ARGC=%d\n", argc);
-	printf("ARGV[0]:%s\n", argv[0]);
-	printf("ARGV[1]:%s\n", argv[1]);
-	printf("ARGV[2]:%s\n", argv[2]);
-	printf("ARGV[3]:%s\n", argv[3]);
+	send_char(pid, '\n');
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,40 +6,54 @@
 /*   By: arcarval <arcarval@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 16:05:49 by arcarval          #+#    #+#             */
-/*   Updated: 2023/02/23 21:05:56 by arcarval         ###   ########.fr       */
+/*   Updated: 2023/02/25 21:13:57 by arcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Libft/libft.h"
 #include <stdio.h>
-//signal macros and kill() prototype
 #include <signal.h>
 
 void	handle_signal(int signal)
 {
-	printf("Inside the handler. Signal number:%d\n", signal);
-	return ;
+	static int	bit;
+	static int	i;
+
+	// When receiving a SIGUSR1, it means it received a 1
+	if (signal == SIGUSR1)
+		i |= (0x01 << bit);
+	// If it receives a SIGUSR2, it means it received a 0
+	if (signal == SIGUSR2)
+		i &= ~(0x01 << bit);
+	bit++;
+	if (bit == 8)
+	{
+		ft_putchar_fd(i, 1);
+		bit = 0;
+		i = 0;
+	}
 }
 
+// Still bugging when receiving a super big string
 int	main(int argc, char **argv)
 {
 	int	pid;
-	int	i = 0;
+	int	i;
 
 	(void)argv;
+	i = 0;
 	if (argc != 1)
 	{
 		printf("Error!\n");
 		return (0);
 	}
 	pid = getpid();
+	printf("%d: LOOP. PID Process: %d\n", i++, pid);
+	signal(SIGUSR1, handle_signal);
+	signal(SIGUSR2, handle_signal);
 	while (1)
 	{
-		printf("%d: LOOP. PID Process: %d\n", i++, pid);
-		printf("ARGV SIZE:%li\n", ft_strlen(argv[1]));
-		signal(SIGUSR1, handle_signal);
-		sleep(1);
+		pause();
 	}
-	printf("PID: %d", pid);
 	return (0);
 }
